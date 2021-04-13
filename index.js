687ecc8de5ef4d1b2fc895498ef066eaa89f2c81
a01750264 <a01750246@itesm.mx>
2021-04-13 14:51:46 -0500

    fix errors

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const jugadorRoutes = require('./routes/Jugador');
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine', 'ejs');
-app.use('/jugador', jugadorRoutes)
+
+app.get('/', (request, response)=>{
+    response.sendFile(path.join(__dirname, 'views'))
+});
 
 const puerto = 8080;
 
-sequelize.sync()
-    .then(resultado=>{
-        console.log("Conexión exitosa");
-        app.listen(puerto, ()=>console.log("Servidor en línea en el puerto 8080"));
-    })
-    .catch(err=>console.log(err));
+app.listen(puerto, ()=>console.log("Servidor en línea en el puerto:", puerto))
